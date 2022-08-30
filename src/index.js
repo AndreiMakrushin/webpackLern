a36c0766ebd2e285844a9a90268d77c903db563f
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-31 02:03:23 +0500

    сделал слайдер

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import modal from "./modules/modal.js"
 import btmanimate from "./modules/btmanimate"
 import validate from "./modules/validate"
 import tabs from "./modules/tabs"
-
+import slider from "./modules/slider"
 
 
 countdowntimer('4 sep 2022');
@@ -13,3 +13,4 @@ modal();
 btmanimate();
 validate();
 tabs();
+slider();
