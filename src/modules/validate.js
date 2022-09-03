8e1614bae0fe9f38f6751e742f4836c6929e10a7
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-09-03 23:07:50 +0500

    сделал домашку

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -1,29 +1,48 @@
 const validate = () =>{
-    const calc = document.querySelector('.calc')
-    const input = calc.querySelectorAll('input')
+    const calcInputs = document.querySelectorAll('.calc-block>input[type = text]')
     const valName = document.querySelectorAll('[placeholder="Ваше имя"]')
     const valEmail = document.querySelectorAll('[placeholder="E-mail"]')
     const valPhone = document.querySelectorAll('[placeholder="Номер телефона"]')
-//-----------------------------------------------------------------------------------------
+    const valMessage = document.querySelector('[placeholder="Ваше сообщение"]')
+    const valPhoneModal = document.getElementById('form3-phone')
+    const valEmailModal = document.getElementById('form3-email')
+//----------------------------------------------------------------------------------------
 
-    valPhone.forEach(valPhone=> valPhone.addEventListener('input', (e) => {
+    calcInputs.forEach((calcInput) => {
+        calcInput.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/\D/g, '')
+        })
+    })
+//----------------------------------------------------------------------------------------
+
+    valPhone.forEach(valPhone => valPhone.addEventListener('input', (e) => {
         e.target.value = e.target.value.replace(/[^+() 0-9 -]+(.*)/, '$1')
     }))
-//-----------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 
-    valEmail.forEach(valEmail=> valEmail.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^@ - _ .! ~ * ' a-z A-Z 0-9 ]+(.*)/, '$1')
-    }))
-//-----------------------------------------------------------------------------------------
+    valPhoneModal.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^+() 0-9 -]+(.*)/, '$1')
+    })
+//-------------------------------------------------------------------------------------------------
 
-    valName.forEach(valName=> valName.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^а-яёА-ЯЁ ]/g,"")
+    valEmail.forEach(valEmail => valEmail.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^@ - _ .! ~ * ' a-z A-Z 0-9]+(.*)/, '$1')
     }))
-//-----------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------
 
-    input.forEach(input => input.addEventListener('input', (e)=>{
-        e.target.value  = e.target.value.replace(/\D/g, '')
+    valEmailModal.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^@ - _ .! ~ * ' a-z A-Z 0-9]+(.*)/, '$1')
+    })
+//---------------------------------------------------------------------------------------------------
+
+    valName.forEach(valName => valName.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яёА-ЯЁ]/g, "")
     }))
+//----------------------------------------------------------------------------------------------------
+
+    valMessage.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яёА-ЯЁ \s]/g, "")
+    })
 
 }
 export default validate
